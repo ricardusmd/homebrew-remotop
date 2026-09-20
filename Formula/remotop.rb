bfa4ee32978918c07ee8c275aa4e69bb611780ab
Ricardo Tamayo <80552235+ricardusmd@users.noreply.github.com>
2026-09-20 12:15:30 -0400

    Update remotop to 0.1.2

--- Formula/remotop.rb
+++ Formula/remotop.rb
@@ -1,9 +1,9 @@
 class Remotop < Formula
   desc "Btop-style monitor for inbound SSH and remote terminal sessions"
   homepage "https://github.com/ricardusmd/remotop"
-  url "https://raw.githubusercontent.com/ricardusmd/remotop/23cc4c10c5bd423f9a05a1ac572ff4a1eb2c41e5/remotop"
-  version "0.1.1"
-  sha256 "8fc1da27d154a0db5d913600d4821646ee4cdaf05833d99372a721b67b540ddf"
+  url "https://raw.githubusercontent.com/ricardusmd/remotop/43e11166a8b1be7feec1196c9441bce9d0b216fe/remotop"
+  version "0.1.2"
+  sha256 "f45512e3cfb85ceac0889648374dd2f7c57d1e5e961c611ae55274ae9145ee39"
   license "MIT"
 
   depends_on "python@3.12"
@@ -13,6 +13,6 @@ class Remotop < Formula
   end
 
   test do
-    assert_match "remotop 0.1.1", shell_output("#{bin}/remotop --version")
+    assert_match "remotop 0.1.2", shell_output("#{bin}/remotop --version")
   end
 end
